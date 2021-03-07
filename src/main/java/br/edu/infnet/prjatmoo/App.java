7782436b056956fbe11cac555cf64bf9ab911ec5
elito <elito@host.docker.internal>
2021-03-07 16:32:16 -0300

    commit inicial

---
+++ src/main/java/br/edu/infnet/prjatmoo/App.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.prjatmoo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
